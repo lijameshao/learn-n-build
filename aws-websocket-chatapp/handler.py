020d829b68f52208da8eea9cf7b459d0850b24cf
lijameshao <lijameshao@yahoo.co.uk>
2023-02-05 21:59:04 -0600

    mock ws

--- aws-websocket-chatapp/handler.py
+++ aws-websocket-chatapp/handler.py
@@ -1,10 +1,26 @@
 import json
+import boto3
+
+client = boto3.client("apigatewaymanagementapi")
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
-    }
+def main(event, context):
+    print(event)
+    route = event["requestContext"]["routeKey"]
+    connection_id = event["requestContext"]["connectionId"]
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+    if route == "$connect":
+        print("User connect")
+    elif route == "$disconnect":
+        print("User DISCONNECT")
+    elif route == "message":
+        message_body = event["requestContext"]["body"]
+        print(f"Message: {message_body}")
+        response_body = {"message": f"Message received: {message_body}"}
+        client.post_to_connection(
+            Data=json.dumps(response_body).encode("utf-8"), ConnectionId=connection_id
+        )
+    else:
+        print(f"Unknown route: {route}")
+
+    return {"statusCode": 200}
