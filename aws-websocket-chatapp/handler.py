8d5a651c21bd68048505b360cf59e8997a8685d5
lijameshao <lijameshao@yahoo.co.uk>
2023-02-07 10:41:43 -0600

    added sample chat app lambdas

--- aws-websocket-chatapp/handler.py
+++ aws-websocket-chatapp/handler.py
@@ -1,3 +1,29 @@
+"""
+TODO:
+- handle WS interrupted cases
+    Delete connection Id
+
+- message action
+    - Write message to DynamoDB, noting message body, user id, chat room id
+- lambda to trigger server side to send message to client when new message is created
+
+Backend added complexities
+- Add authentication in order to identify user
+- Add chat room id
+    - Throws error if chat room is closed
+- message delete action
+    - update message deleted at timestamp
+    - Send message id deleted at action to client
+
+Frontend
+- Initiate connection, sending JWT token
+- Update chat when messages created
+
+Frontend added complexities
+- Disable ability to send message if chat room is closed
+
+"""
+
 import json
 import boto3
 
